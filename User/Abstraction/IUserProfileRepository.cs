333f83f4c6bc56f56a3b282da7d0f1cc93b90b51
Michal Pospěch <Michal.Pospech@alza.cz>
2017-07-21 11:16:26 +0200

    Added structure of User module and created repository interfaces.

---
+++ User/Abstraction/IUserProfileRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using User.DAL.Entities;
+
+namespace User.Abstraction
+{
+    interface IUserProfileRepository
+    {
+        /// <summary>
+        /// Gets user with specified ID
+        /// </summary>
+        /// <param name="id">ID of wanted user</param>
+        /// <returns>UserProfile with specified id if it exsits</returns>
+        UserProfile GetProfile(int id);
+        /// <summary>
+        /// Gets all users
+        /// </summary>
+        /// <returns>Queryable object of all UserProfiles</returns>
+        IQueryable<UserProfile> GetAllProfiles();
+        /// <summary>
+        /// Adds a new UserProfile to database
+        /// </summary>
+        /// <param name="profile">UserProfile to be added</param>
+        /// <returns>Added UserProfile</returns>
+        UserProfile AddUserProfile(UserProfile profile);
+        /// <summary>
+        /// Deletes UserProfile with specified ID from Database
+        /// </summary>
+        /// <param name="id"></param>
+        
+        void DeleteUserProfile(int id);
+        /// <summary>
+        /// Deletes UserProfile with specified ID.
+        /// </summary>
+        /// <param name="profile">Profile to be updated</param>
+        /// <returns>Updated UserProfile</returns>
+        UserProfile UpdateProfile(UserProfile profile);
+    }
+}
---
+++ User/DAL/Entities/UserState.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace User.DAL.Entities
+{
+    class UserState
+    {
+    }
+}
