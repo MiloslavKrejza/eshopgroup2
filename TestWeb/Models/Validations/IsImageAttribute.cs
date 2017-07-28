03f98ab90c599261f86bee7afe313e61aa6b8352
Michal Pospěch <Michal.Pospech@alza.cz>
2017-07-28 19:21:30 +0200

    Added uploading of profile picture. Showing profile picture on Detail page.

---
+++ TestWeb/Models/Validations/IsImageAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestWeb.ViewModels.Validations
+{
+    internal class IsImageAttribute : ValidationAttribute
+    {
+        private string fileName;
+
+
+        protected override ValidationResult IsValid(object value, ValidationContext context)
+        {
+            IFormFile file = (IFormFile)value;
+            if (!file.ContentType.Contains("image"))
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
