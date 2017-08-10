ab789793174e8d194e384eb3bf21581dd5b4eed2
gabriela.suchoparova <gabi.suchoparova@gmail.com>
2017-08-10 13:56:33 +0200

    Commented the code

--- Business/Business/Wrappers/QueryParametersWrapper.cs
+++ Business/Business/Wrappers/QueryParametersWrapper.cs
@@ -7,7 +7,6 @@ namespace Trainee.Business.Business.Wrappers
 {
     public class QueryParametersWrapper
     {
-        //ToDo comment this pls
 
         public int PageNum { get; set; }
         public int PageSize { get; set; }
