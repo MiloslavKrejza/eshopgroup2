6adb05fb70077ddcaf12042bf70b7659f26b6626
miloslav.krejza <miloslav.krejza@VYVOJ114>
2017-08-11 12:49:26 +0200

    C# & Javascript
    1 - 0

--- TestWeb/wwwroot/js/averageRating.js
+++
@@ -1,9 +0,0 @@
-﻿
-//if somebody wanted to have same variable names
-"use strict";
-
-var coolValue = document.getElementById("takeTheValue").innerHTML;
-//making it percentage
-coolValue = coolValue / 5 * 100;
-
-var insertThatFuckingValue = document.getElementById("insertTheValue").style.width = coolValue + "%";
