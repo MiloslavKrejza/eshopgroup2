2c0e6a6cd91a9ed8e7820f152eb059f190ca4c95
alzbeta.kovarikova <alzbeta.kovarikova@alza.cz>
2017-08-22 13:18:10 +0200

    order finished

--- TestWeb/Controllers/OrderController.cs
+++ TestWeb/Controllers/OrderController.cs
@@ -49,7 +49,7 @@ namespace Eshop2.Controllers
             try
             {
                 //user tried to order, but the cart is empty
-                ViewData["emptyCart"] = TempData["emptyCart"];
+                ViewData["emptyOrder"] = TempData["emptyOrder"];
 
 
                 CookieHelper cookieHelper = new CookieHelper(_accessor);
@@ -75,14 +75,16 @@ namespace Eshop2.Controllers
                 if(result.isEmpty)
                 {
                     cart = new List<CartItem>();
-                    ViewData["emptyCart"] = true;
                 }
                 else
                 {
                     cart = result.data;
                 }
                
-
+                if(cart.Count == 0)
+                {
+                    ViewData["emptyCart"] = true;
+                }
 
                 CartViewModel model = new CartViewModel() { Cart = cart };
 
