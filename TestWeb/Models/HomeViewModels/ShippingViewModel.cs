f8f8beb0d78e48571962644ff702249d024a6c32
gabriela.suchoparova <gabi.suchoparova@gmail.com>
2017-08-23 11:37:55 +0200

    Made Shipping and Payment available in home

---
+++ TestWeb/Models/HomeViewModels/ShippingViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Trainee.Business.DAL.Entities;
+
+namespace Eshop2.Models.HomeViewModels
+{
+    public class ShippingViewModel
+    {
+        public List<Shipping> Shipping { get; set; }
+        public List<Payment> Payment { get; set; }
+    }
+}
